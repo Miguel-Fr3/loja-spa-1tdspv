2204950a3ef6e5139638441a6ac9d0ef1fb5bc0a
Miguel_Fr3 <rm99997@fiap.com.br>
2023-10-17 23:01:54 -0300

    Rotas dinamicas, 404, imagens externas

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Link from "next/link";
 export default function cabecalho() {
   return (
-    <div>
+    <header>
         <ul>
-        
-    <li><Link href="/produtos/calca">CALÇA</Link></li>
-    <li><Link href="/produtos/camiseta">CAMISETA</Link></li>
-    <li><Link href="/produtos/tenis">TÊNIS</Link></li>
-    
-  </ul>
-  </div>
+            
+          <li><Link href="/produtos/calca">CALÇA</Link></li>
+          <li><Link href="/produtos/camiseta">CAMISETA</Link></li>
+          <li><Link href="/produtos/tenis">TÊNIS</Link></li>
+      
+        </ul>
+  </header>
   )
 }
