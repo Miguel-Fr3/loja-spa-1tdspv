ea68a62dc975f873c20fb81d045d945184fd5b20
Miguel-Fr3 <miguel.fernandes2004f2@gmail.com>
2023-10-31 22:32:35 -0300

    Sistema de Post validação

--- src/app/api/base/base-users/route.jsx
+++ src/app/api/base/base-users/route.jsx
@@ -1,21 +1,43 @@
-import {promises as fs} from 'fs'
+import {promises as fs} from 'fs';
 import { NextResponse } from 'next/server';
 
-export async function GET(request, {params}){
 
-    const file = await fs.readFile(process.cwd() + '/src/app/api/base/data.json')
+export async function GET(request, {params}) {
+    
+    const file  = await  fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf8');
+
     const id = params.id;
 
-        const usuarios = JSON.parse(file);
-        if(id > 0 && id <= usuarios.usuarios.length){
-            return NextResponse.json(usuarios.usuarios.find((user)=> user.id == id));
-        }else{
-            return id == 0 ? NextResponse.json(usuarios.usuarios) : NextResponse.redirect("http://localhost:3000/api/base/base-users/error")
-        }
+    const usuarios =  await JSON.parse(file);
+    
+    if(id > 0 && id <= usuarios.usuarios.length){
+        return  NextResponse.json(usuarios.usuarios.find((user)=> user.id == id));
+    }else{
+        return id == 0 ? NextResponse.json(usuarios.usuarios) : NextResponse.redirect("http://localhost:3000/error")
+    }
 }
 
 export async function POST(request, response){
-    const file = await fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf-8');
 
-    return NextResponse.json({"STATUS":"OK"})
+    const file  = await  fs.readFile(process.cwd() + '/src/app/api/base/data.json', 'utf8');
+
+    const usuarios = await JSON.parse(file)
+
+    const userRequest = await request.json();
+
+    try{
+        for (let x = 0; x < usuarios.usuarios.length; x++) {
+            const userFile = usuarios.usuarios[x];
+
+            console.log(userFile.email);
+
+            if(userFile.email == userRequest.email && userFile.senha == userRequest.senha){
+                return NextResponse.json({"status":true});
+            }
+        }
+    }catch(error){
+        console.log(error);
+    }
+
+    return NextResponse.json({"status":false});
 }
